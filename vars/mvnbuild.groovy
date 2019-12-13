8ba2869004873106d622e38d9a7af0c4c5632fc6
rsxyz <rsxyz123@gmail.com>
2019-12-12 22:50:47 -0600

    Initial

--- vars/mvnbuild.groovy
+++ vars/mvnbuild.groovy
@@ -3,33 +3,8 @@ def call(body) {
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
     body()
-    pipeline{
-        agent any
-        tools {
-          jdk 'JDK8'
-          maven 'M2'
-        }
-        stages{
-            stage('Checkout'){
-               steps{
-                   git credentialsId: 'github', url: 'https://github.com/rsxyz/HelloJava.git'
-               }
-            }
-            stage('Build') {
-                steps {
-                    sh "mvn clean install"
-                }
-            }
-            stage("Test: SonarQube") {
-                steps {
-                    echo "sonar ..."
-                }
-            }
-            stage('Publish Artifactory') {
-                steps {
-                    echo "artifactory  ..."
-                }
-            }
-        }
+   node {
+        git credentialsId: 'github', url: 'https://github.com/rsxyz/HelloJava.git'
+        sh "mvn clean install"
     }
 }
